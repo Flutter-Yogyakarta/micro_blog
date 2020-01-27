0d66b5fdaf1b9312b943ce53b72142b108360e7c
Helmi Zain <helmizain@gmail.com>
2020-01-27 19:15:41 +0700

    tambah button logout

--- lib/views/user_page.dart
+++ lib/views/user_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class UserPage extends StatefulWidget {
   final FirebaseUser user;
@@ -23,7 +24,14 @@ class _UserPageState extends State<UserPage> {
                 Text('Welcome'),
               ],
             ),
-            Text(widget.user.displayName)
+            Text(widget.user.displayName),
+            Container(
+              child: FlatButton.icon(
+                icon: Icon(MdiIcons.logout),
+                label: Text('Logout'),
+                onPressed: () {},
+              ),
+            )
           ],
         ),
       ),
